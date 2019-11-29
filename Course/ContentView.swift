75f53436655bfafb62b23850c5680a82ebfa6373
Ajinkyarelekar1 <ajinkyarelekar@gmail.com>
2019-11-29 12:25:46 +0530

    Initial Commit

---
+++ Course/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Course
+//
+//  Created by Ajinkya Relekar on 29/11/19.
+//  Copyright © 2019 Ajinkya Relekar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
